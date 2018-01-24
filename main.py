cc63001032d78562f48bb53fdbfe27ff569e6a01
lubien <lubien1996@gmail.com>
2018-01-24 15:39:33 -0300

    Setup simplex_handler

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys
+from utils import nth_x, equation
+from simplex import maximization
+import printer
 
 def main():
   algorithms = ["LU", "Gauss", "Jacobi", "Seidel", "Simplex"]
   print(
     "\n".join(
-      "Digit %i for %s" % (i + 1, algo)
+      "Input %i for %s" % (i + 1, algo)
         for i, algo in enumerate(algorithms)
     )
   )
@@ -41,9 +44,76 @@ def seidel_handler():
   pass
 
 def simplex_handler():
-  pass
+  # Assumes maximization. TODO: minimization
+  prompt = "Number of variables: "
+  n_vars = ask_number(prompt)
+  update_line(prompt + blue(str(n_vars)))
+
+  prompt = "Number of restrictions: "
+  n_restrictions = ask_number(prompt)
+  update_line(prompt + blue(str(n_restrictions)))
+
+  print()
+
+  objective = []
+
+  prompt = "Max. Z = "
+  print(prompt)
+
+  for i in range(1, n_vars + 1):
+    x = ask_number(nth_x(i) + " = ")
+    objective.append(x)
+    erase_line()
+    update_line(prompt + equation(objective))
+
+  restrictions = []
+
+  for i in range(1, n_restrictions + 1):
+    print("Restriction (" + str(i) + "/" + str(n_restrictions) + ")")
+    current_restriction = []
+
+    # Ask coefficients
+
+    for j in range(1, n_vars + 1):
+      x = ask_number(nth_x(j) + " = ")
+      current_restriction.append(x)
+      erase_line()
+      update_line(equation(current_restriction))
+
+    # Ask operation
+
+    op = input("Operation (<=, =, >=)? ")
+
+    if op not in ["<=", "=", ">="]:
+      fail("Invalid operation " + op)
+
+    erase_line()
+    update_line(equation(current_restriction) + " " + op)
+
+    # Ask `b`
+
+    b = ask_number("b = ")
+
+    erase_line()
+    update_line(equation(current_restriction) + " " + op + " " + str(b))
+
+    restrictions.append(current_restriction + [b, op])
+
+  problem = [n_vars, objective, restrictions]
+
+  print()
+
+  gen = maximization(*problem)
+
+  for table in gen:
+    printer.print_simplex_table(3, table)
+    print()
+
+  print()
 
 END_COLOR = '\033[0m'
+CURSOR_UP_ONE = '\x1b[1A'
+ERASE_LINE = '\x1b[2K'
 def red(str):
   return "\033[91m" + str + END_COLOR
 def green(str):
@@ -56,13 +126,10 @@ def fail(str):
   exit(1)
 
 def erase_line():
-  CURSOR_UP_ONE = '\x1b[1A'
-  ERASE_LINE = '\x1b[2K'
-  print(CURSOR_UP_ONE + ERASE_LINE + CURSOR_UP_ONE)
+  sys.stdout.write(CURSOR_UP_ONE + ERASE_LINE)
 
 def update_line(str):
-  erase_line()
-  print(str)
+  print(CURSOR_UP_ONE + ERASE_LINE + str)
 
 def ask_number(prompt):
   received = input(prompt)
